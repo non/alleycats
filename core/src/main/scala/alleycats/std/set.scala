413fcd92af1260a0251881a054df4132853d5809
Erik Osheim <d_m@plastic-idolatry.com>
2015-07-13 18:03:09 -0400

    add Traverse[Set]

--- core/src/main/scala/alleycats/std/set.scala
+++ core/src/main/scala/alleycats/std/set.scala
@@ -1,6 +1,6 @@
 package alleycats.std
 
-import cats.Monad
+import cats.{Applicative, Fold, Monad, Traverse}
 
 object set extends SetInstances
 
@@ -10,5 +10,19 @@ trait SetInstances {
       def pure[A](a: A): Set[A] = Set(a)
       override def map[A, B](fa: Set[A])(f: A => B): Set[B] = fa.map(f)
       def flatMap[A, B](fa: Set[A])(f: A => Set[B]): Set[B] = fa.flatMap(f)
+    }
+
+  implicit val setTraverse: Traverse[Set] =
+    new Traverse[Set] {
+      def foldLeft[A, B](fa: Set[A], b: B)(f: (B, A) => B): B =
+        fa.foldLeft(b)(f)
+      def partialFold[A, B](fa: Set[A])(f: A => Fold[B]): Fold[B] =
+        Fold.partialIterate(fa)(f)
+      def traverse[G[_]: Applicative, A, B](sa: Set[A])(f: A => G[B]): G[Set[B]] = {
+        val G = Applicative[G]
+        sa.foldLeft(G.pure(Set.empty[B])) { (buf, a) =>
+          G.map2(buf, f(a))((sb, b) => sb + b)
+        }
+      }
     }
 }
