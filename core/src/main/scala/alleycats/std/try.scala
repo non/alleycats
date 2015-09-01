5daefafbdff30564144a188a3eeab565b280ef77
Erik Osheim <d_m@plastic-idolatry.com>
2015-08-31 23:01:37 -0400

    Update instances to be compatible with Cats 0.2.0
    This commit also adds some documentation to help
    explain why these instances are in Alleycats.

--- core/src/main/scala/alleycats/std/try.scala
+++ core/src/main/scala/alleycats/std/try.scala
@@ -6,6 +6,23 @@ import scala.util.Try
 object try_ extends TryInstances
 
 trait TryInstances {
+
+  // There are various concerns people have over Try's ability to
+  // satisfy the monad laws. For example, consider the following code:
+  //
+  //     def verify(n: Int): Int =
+  //       if (n == 0) sys.error("nope") else n
+  //
+  //     val x = Try(0).flatMap(verify)
+  //     val y = verify(0)
+  //
+  // The monad laws require that `x` and `y` produce the same value,
+  // but in this case `x` is a `Failure(_)` and `y` is undefined (due
+  // an error being thrown).
+  //
+  // Since `verify` is not a total function, it is arguable whether
+  // this constitutes a law violation, but there is enough concern
+  // that the Monad[Try] instance has ended up here in Alleycats.
   implicit val tryMonad: Monad[Try] =
     new Monad[Try] {
       def pure[A](a: A): Try[A] = Try(a)
